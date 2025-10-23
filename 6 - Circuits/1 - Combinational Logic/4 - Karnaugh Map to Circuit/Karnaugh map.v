cba43baf920900d7c3ef95cbafadbf32e5f307ba
Michael <134752081+UnknownK6912@users.noreply.github.com>
2025-10-23 21:24:30 +0300

    Create Karnaugh map.v

---
+++ 6 - Circuits/1 - Combinational Logic/4 - Karnaugh Map to Circuit/Karnaugh map.v
@@ -0,0 +1,18 @@
+// Exams/m2014 q3
+module top_module (
+    input [4:1] x, 
+    output f );
+
+    assign f = (x[2] & x[4]) | (~x[1] & x[3]) | (~x[2] & x[3]) |  (x[1] & ~ x[3] & ~x[4]) | (~x[2] & ~ x[3] & ~x[4]);
+    
+endmodule
+
+// Exams/2012 q1g
+module top_module (
+    input [4:1] x,
+    output f
+); 
+
+    assign f = (~x[1] & x[3]) | (~x[2] & ~x[4]) | (x[2] & x[3] & x[4]);
+    
+endmodule
